fac7ea870e3d114c8ce41db357129da6fd34c309
HJ2411 <hjnj1004@gmail.com>
2026-03-12 18:08:40 +0900

    feat(analytics): GTM(Google Tag Manager) 스크립트 추가
    - root layout에 GTM 스크립트 및 noscript fallback 삽입
    - GTM 컨테이너 ID: GTM-KC4N5LM8

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Oswald, Noto_Sans_KR } from "next/font/google";
 import localFont from "next/font/local";
 import AppShell from "@/components/AppShell/AppShell";
@@ -61,9 +62,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <Script id="gtm" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KC4N5LM8');`}
+      </Script>
       <body
         className={`${oswald.variable} ${notoSansKR.variable} ${pretendard.variable}`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KC4N5LM8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <AppShell>{children}</AppShell>
         <Footer />
         <KakaoFloat />
