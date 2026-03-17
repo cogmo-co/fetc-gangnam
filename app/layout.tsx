90425099252ecd011577de81d695e6efb98acd01
HJ2411 <hjnj1004@gmail.com>
2026-03-17 13:31:57 +0900

    feat(analytics): Microsoft Clarity 스크립트 추가
    - root layout에 MS Clarity 추적 스크립트 삽입
    - GTM/Clarity 스크립트에 JSX 주석 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,12 +62,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      {/* Google Tag Manager */}
       <Script id="gtm" strategy="afterInteractive">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-KC4N5LM8');`}
+      </Script>
+      {/* Microsoft Clarity */}
+      <Script id="ms-clarity" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window,document,"clarity","script","vv0e6snlxl");`}
       </Script>
       <body
         className={`${oswald.variable} ${notoSansKR.variable} ${pretendard.variable}`}
